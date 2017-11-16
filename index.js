f858a13cffe975d91df0e2bd9475d80b1b280706
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-16 23:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
 function happyHolidays(string){return "Happy holidays!"}
+
 function happyHolidaysTo(name){return 'Happy holidays, '+ name +'!'}
 function happyHolidaysTo(holiday, name){return 'Happy '+ ${holiday}+', ' ${name}+ '!'}
